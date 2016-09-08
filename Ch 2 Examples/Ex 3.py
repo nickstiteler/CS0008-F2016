65e5dfa5c3f79a5467ad2dbb45fab77cea6e934e
nickstiteler <njs74@pitt.edu>
2016-09-08 14:04:35 -0400

    Executed and completed Exercise 3 from Chapter 2

---
+++ Ch 2 Examples/Ex 3.py
@@ -0,0 +1,17 @@
+#
+#Template for code submission
+# name       : Nicholas Stiteler
+# email      : njs74@pitt.edu
+# date       : 9/8/16
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+#Description:
+# This program will take your area in meters squared and convert it to acres
+3
+#Area in meters square is entered here
+x = float(input('enter_area'))
+print(x)
+#Area is converted to acres through equation
+y=x/4046.8564224
+print(y,'acres')
