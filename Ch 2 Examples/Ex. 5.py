ef9374cae4d2c52fe0d5a1aea7869706882b834d
nickstiteler <njs74@pitt.edu>
2016-09-08 22:42:39 -0400

    Excercise 5 for chapter 2 completed

---
+++ Ch 2 Examples/Ex. 5.py
@@ -0,0 +1,29 @@
+#
+#Template for code submission
+# name       : Nicholas Stiteler
+# email      : njs74@pitt.edu
+# date       : 9/8/16
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Distance a car travelled program
+# Example: Chapter 2 Exercise 5
+3
+#I'm unsure how to properly use \n for next line so I had to print the long way
+#Identifying variables
+speed = 90
+t1 = 6
+t2 = 10
+t3 = 15
+t4 = 29/12
+#Equations for each distance
+d1 = float(speed*t1)
+d2 = float(speed*t2)
+d3 = float(speed*t3)
+d4 = float(speed*t4)
+#print for each distance
+print('ditance1',d1)
+print('distance2',d2)
+print('distance3',d3)
+print('distance4',d4)
