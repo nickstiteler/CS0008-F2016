8c1440715d4f2b1ceca108e3fa065a76b8391beb
nickstiteler <njs74@pitt.edu>
2016-09-15 09:27:37 -0400

    Excercise 9 for chapter 2 completed

---
+++ Ch 2 Examples/Ex 9.py
@@ -0,0 +1,19 @@
+#
+#Template for code submission
+# name       : Nicholas Stiteler
+# email      : njs74@pitt.edu
+# date       :
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Farenheit to celcius program
+# Example: CH 2 EX 9
+3
+# Notes:
+# any notes to the instructor and/or TA goes here
+# ...and now let's program with Python
+
+F = float(input('Enter temperature in farenheit'))
+C = 5*(F-32)/9
+print(C,"degrees celcius")
