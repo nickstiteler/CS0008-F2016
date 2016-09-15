686cf64f657905fe8e10538f9a6451d122103439
nickstiteler <njs74@pitt.edu>
2016-09-15 09:53:48 -0400

    Added comments and explanation for lines of code

--- Ch 2 Examples/Ex 9.py
+++ Ch 2 Examples/Ex 9.py
@@ -14,6 +14,9 @@
 # any notes to the instructor and/or TA goes here
 # ...and now let's program with Python
 
+# Intial equation for the input of the degrees in farenheit
 F = float(input('Enter temperature in farenheit'))
+# Equation for converting degrees farenheit to degrees celcius
 C = 5*(F-32)/9
+# Printing of the final converted value of degrees celcius
 print(C,"degrees celcius")
