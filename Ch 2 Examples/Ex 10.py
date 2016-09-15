e5bebae1678d32d8ec2e95feb0072d2d572143b4
nickstiteler <njs74@pitt.edu>
2016-09-15 09:50:02 -0400

    Completed ch 2 ex 10

--- Ch 2 Examples/Ex 10.py
+++ Ch 2 Examples/Ex 10.py
@@ -7,10 +7,18 @@
 # instructor : Max Novelli (man8@pitt.edu)
 #
 # Description:
-# Description goes here
-# Example:
+# Program that asks for the number of cookies wanted and gives you the amount of ingredients needed
+# Example: CH 2 EX 10
 3
 # Notes:
 # any notes to the instructor and/or TA goes here
 # ...and now let's program with Python
 
+# Intial equation for the input of the number of cookies wanted
+c = float(input('Number of cookies wanted'))
+# Equation for each individual ingredient
+s = 300*c/48
+b = 240*c/48
+f = 330*c/48
+# Printing of the amount of ingredients required
+print(s,'grams of sugar,',b,'grams of butter, and',f,'grams of flour')
