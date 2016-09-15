14dfebbad9a0cac083f3f242af8f6ec93cc9000d
nickstiteler <njs74@pitt.edu>
2016-09-15 12:50:18 -0400

    In class example 9/15/16

---
+++ In class work 9-15-16/In class Ex.py
@@ -0,0 +1,34 @@
+#
+# Template for code submission
+# name       : Nicholas Stiteler
+# email      : njs74@pitt.edu
+# date       :
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Example problem
+# Example: Recitation Problem
+3
+# Notes:
+# any notes to the instructor and/or TA goes here
+# ...and now let's program with Python
+
+# Input for any numerical input and establishes variable "x"
+x = float(input('Enter a number'))
+
+# Multiplies x by three if it is positive and then prints x
+if(x>0):
+    y = x*3
+    print(y)
+
+# Prints 'What a cool number!' if x is equal to 0
+elif(x==0):
+    print('What a cool number!')
+
+# Adds 5 to x if x is negative then prints x
+elif(x<0):
+    y = x+5
+    print(y)
+
+
