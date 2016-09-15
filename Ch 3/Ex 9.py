e753dad93fa0a6565066e4bb32e78646f7d8d92e
nickstiteler <njs74@pitt.edu>
2016-09-15 15:49:10 -0400

    CH 3 EX 9

---
+++ Ch 3/Ex 9.py
@@ -0,0 +1,47 @@
+#
+# Template for code submission
+# name       : Nicholas Stiteler
+# email      : njs74@pitt.edu
+# date       :
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# 0 to 36 roulette wheel
+# Example: Starting with Python, Chapter 3, Exercise 9
+3
+# Notes:
+# any notes to the instructor and/or TA goes here
+# ...and now let's program with Python
+
+
+number = input('Give me the number')
+
+number = int(number)
+# I test
+if(number<0 or number>36):
+    print('give me a number!!')
+    return
+
+if number == 0:
+    color = 'green'
+elif cond1:
+    if (number>=1 and number<=10):
+        or\
+        (number>=19 and number=<28):
+    # Testing for even or odd
+    if number %2==0:
+        color = 'black'
+    else:
+        color = 'red'
+elif number>=11 and number<=18
+        if number %2==0
+            color = 'red'
+        else:
+            'black'
+
+
+
+
+
+
