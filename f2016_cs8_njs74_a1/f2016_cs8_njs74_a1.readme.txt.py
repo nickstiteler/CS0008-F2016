70c496c6c865fb5553c241ac16c1684386ecb89a
nickstiteler <njs74@pitt.edu>
2016-09-28 19:26:06 -0400

    Started assignment 1 and filled out the readme file

---
+++ f2016_cs8_njs74_a1/f2016_cs8_njs74_a1.readme.txt.py
@@ -0,0 +1,18 @@
+#
+# Template for code submission
+# name       : Nicholas Stiteler
+# email      : njs74@pitt.edu
+# date       : 9/30/16
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# The first assignment. Asking to create a program that satisfies the customers
+# request, like calculating and converting the gas, consumption, distance, and
+# gas consumption rating
+# Example: Assignment 1
+3
+# Notes:
+# There are two main paths that are required to create this program. One if the
+# input is in metric and another if it is in US Customary.
+
