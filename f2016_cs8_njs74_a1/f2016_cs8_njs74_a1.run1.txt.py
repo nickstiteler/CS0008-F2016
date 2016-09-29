07655606abc5b1867f7f747abddd782c54e2116d
nickstiteler <njs74@pitt.edu>
2016-09-29 12:13:17 -0400

    Assignment 1 Final Copy 3

---
+++ f2016_cs8_njs74_a1/f2016_cs8_njs74_a1.run1.txt.py
@@ -0,0 +1,11 @@
+Enter 1 for usc units and 2 for metric units1
+units accepted
+Please enter the distance driven60000
+Please enter the gas used543
+	 	 	 	 		 	 USC 	 	 	 	 Metric
+Distance______________:	  60000.000 miles	  96560.400 Km
+Gas___________________:	    543.000 gallons	   2055.478 liters
+Consumption___________:	    110.497 mpg	 	      2.129 l/100Km
+Gas Consumption Rating: Excellent
+
+Process finished with exit code 0
