7c0629f09f186df4778456527b332a422ba059b0
nickstiteler <njs74@pitt.edu>
2016-10-27 11:40:23 -0400

    Began Assignment # 2

--- f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
+++ f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
@@ -3,13 +3,15 @@ case = 'yes'
 while case=='yes':
     file_name = input(str('Enter the file name.'))
     def processFile(file_name):
+        file_name = open(file_name,'r')
         numline = 0
-        distance = 0
-        for line in file_name:
+        partial_distance = 0
+        for line in file_name: numline +=1
+        for distance in file_name: partial_distance
 
 
 
-        
 
     case = input(str('Do you have any more files? Enter "yes" or "no"'))
-def printfile():
+def printKV(key,value,klen = 0)
+    
