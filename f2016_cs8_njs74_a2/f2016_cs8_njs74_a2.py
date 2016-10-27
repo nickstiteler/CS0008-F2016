7e8bad56b884713cfd35199a51c3ec63e563a87c
nickstiteler <njs74@pitt.edu>
2016-10-27 15:50:14 -0400

    Stuff max did in class

--- f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
+++ f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
@@ -2,28 +2,42 @@
 case = True
 while (case):
 
-    file_name = input('Enter the file name, or enter "quit" or "q" if you have no more files.')
+    file_name = input('Enter the file name, or enter "quit", "q", or an "empty string" if you have no more files.')
     def processFile(file_name):
         file_name = open(file_name, 'r')
 
-
+        partial_distance = 0
         numline = 0
         for line in file_name:
             numline +=1
-        print(numline)
 
-        total_distance = 0
-        for distance in file_name:
-            partial_distance = 0
-            for distance in file_name:
-                partial_distance = partial_distance.split(',')
-            print(partial_distance)
+            line = line.rstip("\n")
+            temp = line.split(",")
+            distance = float(temp[1])
+            partial_distance = distance
+
+
         file_name.close()
-
 
 
     if (file_name == 'quit' or file_name == 'q'):
         case = False
+
+    def printKV(key, value,klen=0):
+        KL = max(len(key),klen)
+        if(isinstance(value,str)):
+            FS = '20s'
+        elif isinstance(value,float):
+            FS = '10.3f'
+
+        print(format(key,str(KL)+'s'))
+
+        format(value,FS)
+
+
+    def printKV('Partial Total Number of lines',num_line)
+
+    def printKV('Partial Distance Ran',partial_distance)
 
 
     processFile(file_name)
