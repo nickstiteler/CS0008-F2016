982ec9ffb1351b2213576e235acb09c4747e337e
nickstiteler <njs74@pitt.edu>
2016-10-27 10:04:17 -0400

    Began Assignment # 2

--- f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
+++ f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
@@ -1,9 +1,15 @@
-#http://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
-while True:
-    try:
-        file_name = str(input('Enter the file name'))
-    except inproperfilename:
-        print('Sorry, this file name is not acceptable')
+
+case = 'yes'
+while case=='yes':
+    file_name = input(str('Enter the file name.'))
+    def processFile(file_name):
+        numline = 0
+        distance = 0
+        for line in file_name:
 
 
 
+        
+
+    case = input(str('Do you have any more files? Enter "yes" or "no"'))
+def printfile():
