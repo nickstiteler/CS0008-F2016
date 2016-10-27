4461dfcbbfebd4c2557d32e6a858a9445db95ca2
nickstiteler <njs74@pitt.edu>
2016-10-27 14:29:56 -0400

    Finished numline and began distance

--- f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
+++ f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
@@ -1,16 +1,29 @@
 
-case = 'yes'
-while case=='yes':
-    file_name = input(str('Enter the file name.'))
+case = True
+while (case):
+
+    file_name = input('Enter the file name, or enter "quit" or "q" if you have no more files.')
     def processFile(file_name):
-        file_name = open(file_name,'r')
+        file_name = open(file_name, 'r')
+
+
         numline = 0
-        partial_distance = 0
-        for line in file_name: numline +=1
-        for distance in file_name: partial_distance
+        for line in file_name:
+            numline +=1
+        print(numline)
+
+        total_distance = 0
+        for distance in file_name:
+            partial_distance = 0
+            for distance in file_name:
+                partial_distance += file_name.split(',')
+            print(partial_distance)
+        file_name.close()
 
 
 
+    if (file_name == 'quit' or file_name == 'q'):
+        case = False
 
-    case = input(str('Do you have any more files? Enter "yes" or "no"'))
-def printKV(key,value,klen = 0)
+
+    processFile(file_name)
