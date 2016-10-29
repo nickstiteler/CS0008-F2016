17d4e5bb923d0df3b5fc6ee0809a033204ee07d3
nickstiteler <njs74@pitt.edu>
2016-10-29 14:51:58 -0400

    Stuff max did in class

--- f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
+++ f2016_cs8_njs74_a2/f2016_cs8_njs74_a2.py
@@ -1,20 +1,25 @@
 
+totaldistance = 0
+totalline = 0
 case = True
 while (case):
 
     file_name = input('Enter the file name, or enter "quit", "q", or an "empty string" if you have no more files.')
+
     def processFile(file_name):
         file_name = open(file_name, 'r')
 
         partial_distance = 0
         numline = 0
         for line in file_name:
-            numline +=1
 
-            line = line.rstip("\n")
+
+            line = line.rstrip("\n")
             temp = line.split(",")
             distance = float(temp[1])
-            partial_distance = distance
+            partial_distance += distance
+            numline += 1
+
 
 
         file_name.close()
@@ -22,24 +27,29 @@ while (case):
 
 
 
-        def printKV(key, value,klen=0):
+        def printKV(key,value,klen=0):
             KL = max(len(key),klen)
             if(isinstance(value,str)):
                 FS = '20s'
             elif isinstance(value,float):
                 FS = '10.3f'
-
+            printKV('', numline)
+            printKV('', partial_distance)
+            totaldistance += partial_distance
+            totalline += numline
         print(format(key,str(KL)+'s'))
 
         format(value,FS)
 
 
-        printKV('Partial Total Number of lines',num_line)
-
-        printKV('Partial Distance Ran',partial_distance)
 
 
-    processFile(file_name)
+    numline,partial_distance = processFile(file_name)
+
 
     if (file_name == 'quit' or file_name == 'q'):
         case = False
+
+#        printKV('Partial Total Number of lines',numline)
+
+#        printKV('Partial Distance Ran',partial_distance)
